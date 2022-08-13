f471fb06d9fa6d8068358f99a27470ac35f3855f
Nick Rombach <Nick@Nicks-MBP.routerlogin.net>
2022-08-12 22:32:04 -0400

    4. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,9 +9,16 @@ contract Token {
 	uint256 public decimals = 18;
 	uint256 public totalSupply; // = 1000000 * (10**decimals); //WEI is 1000000 x 10^18
 
+		//track balances, stores value directly on blockchain
+		//database on blockchain, have ID to write key and get value
+		//mapping allows key value pair storage
+		//accessable outside
+	mapping (address => uint256) public balanceOf;
+
 	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
 		name = _name; //take value passed in and assign to state value
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals);
+		balanceOf[msg.sender] = totalSupply; //dynamic write global sender ID key to get total supply data using mapping
 	}
 }
--- test/Token.js
+++ test/Token.js
@@ -6,11 +6,16 @@ const tokens = (n) => {
 }
 
 describe('Token', ()=> {
-	let token
+	let token,
+		accounts,
+		deployer
 	
 	beforeEach( async () => {
 		const Token = await ethers.getContractFactory('Token')
 		token = await Token.deploy('Botany', 'BOT', '1000000') //get deployed instance
+	
+		accounts = await ethers.getSigners()
+		deployer = accounts[0]
 	})
 
 	describe('Deployment', ()=> {
@@ -33,6 +38,11 @@ describe('Token', ()=> {
 
 		it('has correct total supply', async ()=> {
 			expect(await token.totalSupply()).to.equal(tokens(totalSupply)) //matchers like .to and .equal
+		})
+
+		it('assigned total supply to deployer', async ()=> {
+			//console.log(deployer.address)
+			expect(await token.balanceOf(deployer.address)).to.equal(tokens(totalSupply))
 		})
 
 	})
